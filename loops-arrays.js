114953379bc7e0891eb1ee923b9a06dc34044672
ontwerup <info@ontwerup.nl>
2021-04-12 10:32:52 +0200

    ready

--- loops-arrays.js
+++ loops-arrays.js
@@ -17,3 +17,45 @@ for (let i = 0; i < names.length; i++) {
 // console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
+
+
+// ==========================================
+// Opdracht 2
+// Schrijf een script dat ieder getal in bovenstaande array met 3 vermenigvuldigd.
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// ==========================================
+//
+const numbers = [2, 4, 5, 29, 38];
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = numbers[i] * 3;
+}
+console.log(numbers);
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(numbers) geeft: [2, 4, 5, 29, 38];
+// Na jouw script zie je de aangepaste waardes:
+// console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
+
+
+// ==========================================
+// Opdracht 3
+// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array het volume uitrekent.
+// Het volume is lengte x breedte x hoogte
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// ==========================================
+
+const squares = [30, 2, 8, 24, 11];
+for (let i = 0; i < squares.length; i++) {
+    squares[i] = squares[i] * squares[i] * squares[i];
+}
+console.log(squares);
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(numbers) geeft: [30, 2, 8, 24, 11];
+// Na jouw script zie je de aangepaste waardes:
+// console.log(numbers) geeft: [27000, 8, 512, 13824, 1331];
+
+
+
