e9e0da29730dbf33977321ddc781d88b2c50fff0
ontwerup <info@ontwerup.nl>
2021-04-12 09:46:29 +0200

    loops opdrachten 1 en 2

--- loops.js
+++ loops.js
@@ -9,7 +9,6 @@ for (let i = 0; i < 3; i++) {
     console.log("Hoera!");
 }
 
-
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
 // Verwachte uitkomsten:
@@ -36,43 +35,3 @@ for (let i = 0; i < 5; i++) {
 }
 
 
-// ==========================================
-// Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
-// Verwachte uitkomsten:
-// 0 is even
-// 1 is oneven
-// 2 is even
-// 3 is oneven
-// .... etc.
-// 15 is oneven
-
-// Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
-// ==========================================
-
-// for loop van 0 tot 15 loop
-for (let i = 0; i < 16; i++) {
-    // voor elk getal even of oneven tonen
-    if (i % 2 === 0) {
-        console.log(i + " is even")
-    } else {
-        console.log(i + " is oneven")
-    }
-}
-
-
-// ==========================================
-// [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
-// Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal te staan.
-// Verwachte uitkomsten:
-// 0
-// 1
-// 2
-// > 3
-// > 4
-// > 5
-// >> 6
-// >> 7
-// >> 9
-// ==========================================
-
-// deze kom ik niet uit  :(
