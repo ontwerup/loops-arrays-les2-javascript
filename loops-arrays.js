dfb4bca5d68f95f5e375c544d64bf8175d735fd4
ontwerup <info@ontwerup.nl>
2021-04-12 10:12:03 +0200

    loops and arrays opdracht 1

---
+++ loops-arrays.js
@@ -0,0 +1,19 @@
+// ==========================================
+// Opdracht 1
+// Schrijf een script dat iedere naam in bovenstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
+// ==========================================
+
+const names = ["Henk", "Piet", "Fred", "Joop"];
+for (let i = 0; i < names.length; i++) {
+    // console.log (`${names[i]}je`);
+    // Backticks(``) kun je strings aan elkaar koppelen en interpoleren, zodat onze code veel schoner is.
+
+    console.log(names[i] + "je");
+}
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
+// Na jouw script zie je de aangepaste waardes:
+// console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
